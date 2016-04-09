84d4c1daa949e9adccf103b44dfb7eaae7c38b8c
mikemcauther <mcauther312@yahoo.com.tw>
2016-04-09 10:26:57 +0800

    Add capability and modify company name .

--- App/App/Properties/AssemblyInfo.cs
+++ App/App/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WhiteBoardScreenShare")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("QISDA")]
 [assembly: AssemblyProduct("WhiteBoardScreenShare")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
